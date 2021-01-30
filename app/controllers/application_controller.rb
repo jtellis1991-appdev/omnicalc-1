82124ec0c51ed2912e31ca385f1fb9395bbd76ff
jtellis1991 <joshuaellis1991@gmail.com>
2021-01-30 20:22:54 +0000

    complete square and square root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,37 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+   
+
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square = @num ** 2
+
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_square_root_form
+   
+
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @num = params.fetch("number").to_f
+    @root = Math.sqrt(@num)
+
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min")
+    @upper = params.fetch("user_max")
+    @result = rand(@lower..@uper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
 end
